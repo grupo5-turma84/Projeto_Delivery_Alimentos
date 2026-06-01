388e99c5c37275fe872371be400a637c3d7133ae
jackeline <jackelinepessoa0@gmail.com>
2026-06-01 14:32:39 -0300

    enviando repository e service

--- src/main/java/com/delivery/controller/ProdutoController.java
+++ src/main/java/com/delivery/controller/ProdutoController.java
@@ -1,65 +1,69 @@
 package com.delivery.controller;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpStatus;
+
 import jakarta.validation.Valid;
 
 import com.delivery.model.Produto;
 import com.delivery.repository.ProdutoRepository;
+import com.delivery.service.ProdutoService;
 
 @RestController
 @RequestMapping("/produtos")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-
 public class ProdutoController {
-	  @Autowired
-	    private ProdutoRepository repository;
 
-	    @GetMapping
-	    public ResponseEntity<List<Produto>> getAll() {
-	        return ResponseEntity.ok(repository.findAll());
-	    }
+    @Autowired
+    private ProdutoRepository repository;
 
-	    @GetMapping("/{id}")
-	    public ResponseEntity<Produto> getById(@PathVariable Long id) {
+    @Autowired
+    private ProdutoService service;
 
-	        return repository.findById(id)
-	                .map(resposta -> ResponseEntity.ok(resposta))
-	                .orElse(ResponseEntity.notFound().build());
-	    }
+    @GetMapping("/saudaveis")
+    public ResponseEntity<List<Produto>> getProdutosSaudaveis() {
+        return ResponseEntity.ok(service.recomendarProdutosSaudaveis());
+    }
 
-	    @GetMapping("/nome/{nome}")
-	    public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome) {
+    @GetMapping
+    public ResponseEntity<List<Produto>> getAll() {
+        return ResponseEntity.ok(repository.findAll());
+    }
 
-	        return ResponseEntity.ok(
-	                repository.findAllByNomeContainingIgnoreCase(nome));
-	    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Produto> getById(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(resposta -> ResponseEntity.ok(resposta))
+                .orElse(ResponseEntity.notFound().build());
+    }
 
-	    @PostMapping
-	    public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto) {
+    @GetMapping("/nome/{nome}")
+    public ResponseEntity<List<Produto>> getByNome(@PathVariable String nome) {
+        return ResponseEntity.ok(repository.findAllByNomeContainingIgnoreCase(nome));
+    }
 
-	        return ResponseEntity.status(HttpStatus.CREATED)
-	                .body(repository.save(produto));
-	    }
+    @PostMapping
+    public ResponseEntity<Produto> post(@Valid @RequestBody Produto produto) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(repository.save(produto));
+    }
 
-	    @PutMapping
-	    public ResponseEntity<Produto> put(@RequestBody Produto produto) {
+    @PutMapping
+    public ResponseEntity<Produto> put(@Valid @RequestBody Produto produto) {
+        return ResponseEntity.ok(repository.save(produto));
+    }
 
-	        return ResponseEntity.ok(repository.save(produto));
-	    }
-
-	    @DeleteMapping("/{id}")
-	    public ResponseEntity<?> delete(@PathVariable Long id) {
-
-	        return repository.findById(id)
-	                .map(resposta -> {
-	                    repository.deleteById(id);
-	                    return ResponseEntity.noContent().build();
-	                })
-	                .orElse(ResponseEntity.notFound().build());
-	    }
-
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable Long id) {
+        return repository.findById(id)
+                .map(resposta -> {
+                    repository.deleteById(id);
+                    return ResponseEntity.noContent().build();
+                })
+                .orElse(ResponseEntity.notFound().build());
+    }
 }
--- src/main/java/com/delivery/controller/UsuarioController.java
+++ src/main/java/com/delivery/controller/UsuarioController.java
@@ -5,12 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.validation.annotation.Validated;
 import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.*;
-
 import com.delivery.model.Usuario;
 import com.delivery.repository.UsuarioRepository;
 
